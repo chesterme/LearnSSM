a1f45750fa3a50f62b023a67c8059d7cc19c976f
chesterme <37114017+chesterme@users.noreply.github.com>
2019-09-23 17:04:52 +0800

    反射方法

--- src/test/java/ch02/ReflectServiceImplTest.java
+++ src/test/java/ch02/ReflectServiceImplTest.java
@@ -1,5 +1,8 @@
 package ch02;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class ReflectServiceImplTest {
 
     public ReflectServiceImpl getInstance(){
@@ -13,10 +16,27 @@ public class ReflectServiceImplTest {
         return obj;
     }
 
+    public Object reflectMethod(){
+        Object returnObj = null;
+        ReflectServiceImpl target = new ReflectServiceImpl();
+        try{
+            Method method = ReflectServiceImpl.class.getMethod("sayHello", String.class);
+            returnObj = method.invoke(target, "张三");
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return returnObj;
+    }
+
     public static void main(String[] args){
         ReflectServiceImplTest test = new ReflectServiceImplTest();
         ReflectServiceImpl instance = test.getInstance();
         instance.sayHello("张三");
+        test.reflectMethod();
     }
 
 }
